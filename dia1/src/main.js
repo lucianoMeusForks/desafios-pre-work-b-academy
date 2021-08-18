0336f34e5e576014f60849628224ee198256ac2f
Luciano Baraúna <baraunaluciano@gmail.com>
2021-08-17 22:00:59 -0300

    resolvido - ex04

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,6 +1,19 @@
 import './style.css'
 
-document.querySelector('[data-js="app"]').innerHTML = `
+const $app = document.querySelector('[data-js="app"]')
+const $btnLink = document.querySelector('[data-js="btnLink"]')
+
+$app.innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+
+$btnLink.addEventListener('click', (evt) => {
+  evt.preventDefault()
+  $app.classList.toggle('hidden')
+
+  // Apresenta texto dependendo da classe
+  $app.classList.contains('hidden')
+    ? evt.target.innerHTML = 'Ver mensagem'
+    : evt.target.innerHTML = 'Esconder mensagem'
+})
