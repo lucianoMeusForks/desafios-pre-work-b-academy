e90f719e44a166cbedca87d44532e5dba209dbca
Luciano Baraúna <baraunaluciano@gmail.com>
2021-08-22 15:37:43 -0300

    incluindo novos utils

--- dia3/src/utils.js
+++ dia3/src/utils.js
@@ -2,4 +2,12 @@ function $ (element) {
   return document.querySelector(element)
 }
 
-export { $ }
+function $all (element) {
+  return document.querySelectorAll(element)
+}
+
+function $create (element) {
+  return document.createElement(element)
+}
+
+export { $, $all, $create }
